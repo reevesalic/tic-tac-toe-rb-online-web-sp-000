ab9ff0820db83ac68723c94e8cf25d67bfb62325
Alicia Reeves <reevesalic@gmail.com>
2020-06-11 15:11:45 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,4 @@
-board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+
 def display_board(board)
 
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -7,54 +7,54 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
-board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 
-def input_to_index(user_input = "X")
-  user_input.to_i - 1
-  end
-
-def move(board, index, win = "X")
-  board[index] = win
-end
-
-def current_player(board)
-  turn_count(board) % 2 == 0? "X" : "O"
-end
-
-  def turn(board)
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
-  if valid_move?(board, index)
-    move(board, index, player(board))
-    display_board(board)
-  else
-    turn(board)
-  end
-end
-
-def turn_count(board)
-  counter = 0
-  board.each {|space|
-    if space == "X" || space == "O"
-      counter += 1
+  def input_to_index(user_input)
+    user_input.to_i - 1
     end
-  }
-  counter
-end
 
+  def move(board, index, win)
+    board[index] = win
+    end
+
+  def turn_count(board)
+    counter = 0
+     board.each {|space|
+      if space == "X" || space == "O"
+         counter += 1
+        end
+      }
+      counter
+    end
+
+ def current_player(board)
+      turn_count(board) % 2 == 0? "X" : "O"
+    end
+
+ def turn(board)
+      puts "Please enter 1-9:"
+      user_input = gets.strip
+      index = input_to_index(user_input)
+      if valid_move?(board, index)
+        move(board, index, current_player(board))
+        display_board(board)
+      else
+        turn(user_input)
+      end
+    end
 
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 def valid_move?(board, index)
-  if !position_taken?(board, index) && (index).between?(0,8)
+  if !position_taken?(board, index) && index.between?(0,8)
     return true
   else
-    return false
+    false
   end
 end
+#turn(user_input)
+
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,4,8], [2,4,6], [0,3,6], [1,4,7], [2,5,8]]
 
@@ -66,9 +66,8 @@ def won?(board)
 
     end
   end
-  false
+   return false
 end
-
 def full?(board)
   if board.any? {|index| index == nil || index == " "}
     return false
@@ -78,12 +77,8 @@ def full?(board)
 end
 
 def draw?(board)
-  if !won?(board) && full?(board)
-    return true
-  else !full?(board) && !won?(board)
-    return false
-  end
-end
+  !won?(board) && full?(board)
+ end
 
 def over?(board)
   if draw?(board) || won?(board) || full?(board)
@@ -93,8 +88,6 @@ def over?(board)
   end
 end
 
-# The `#winner` method should accept a board and return the token, "X" or "O" that
-# has won the game given a winning board.
 def winner(board)
   winning_combo = won?(board)
   if winning_combo
@@ -103,21 +96,14 @@ def winner(board)
 end
 
 def play(board)
-  counter = 0
-  until counter == 9
-  turn(board)
-  counter += 1
-  end
-end
 
-def play(board)
-  until over?(board)
-    turn(board)
-  end
-  if won?(board)
-    winner(board) == "X" || winner(board) == "O"
+  #counter = 0
+  #until counter == 9
+   # counter += 1
+    turn(board) until over?(board)
+    if won?(board)
     puts "Congratulations #{winner(board)}!"
-  else draw?(board)
+  elsif draw?(board)
     puts "Cat's Game!"
+    end
   end
-end
